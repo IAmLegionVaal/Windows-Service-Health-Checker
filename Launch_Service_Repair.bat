8a88d50482f3e000a917b67fc10fb040eda295eb
IAmLegion <37851466+IAmLegionVaal@users.noreply.github.com>
2026-06-22 10:45:09 +0200

    Fix service launcher menu control flow

--- Launch_Service_Repair.bat
+++ Launch_Service_Repair.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "Unblock-File -LiteralPath '%~dp0Windows_Service_Repair_Toolkit.ps1' -ErrorAction SilentlyContinue"
 
 :menu
 cls
@@ -19,7 +20,7 @@ echo   0. Exit
 echo ============================================================
 set /p CHOICE=Select an option: 
 
-if "%CHOICE%"=="1" set ARGS=&goto run
+if "%CHOICE%"=="1" goto diagnose
 if "%CHOICE%"=="2" goto safe
 if "%CHOICE%"=="3" goto start
 if "%CHOICE%"=="4" goto restart
@@ -30,45 +31,49 @@ if "%CHOICE%"=="8" goto terminate
 if "%CHOICE%"=="0" goto end
 goto menu
 
+:diagnose
+set /p SERVICE=Service name to inspect (leave blank for inventory only): 
+if "%SERVICE%"=="" powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1"
+if not "%SERVICE%"=="" powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%"
+goto complete
+
 :safe
 set /p SERVICE=Service name: 
-set ARGS=-ServiceName "%SERVICE%" -RepairAllSafe
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -RepairAllSafe
+goto complete
 
 :start
 set /p SERVICE=Service name: 
-set ARGS=-ServiceName "%SERVICE%" -StartService
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -StartService
+goto complete
 
 :restart
 set /p SERVICE=Service name: 
-set ARGS=-ServiceName "%SERVICE%" -RestartService
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -RestartService
+goto complete
 
 :stop
 set /p SERVICE=Service name: 
-set ARGS=-ServiceName "%SERVICE%" -StopService
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -StopService
+goto complete
 
 :deps
 set /p SERVICE=Service name: 
-set ARGS=-ServiceName "%SERVICE%" -StartDependencies
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -StartDependencies
+goto complete
 
 :startup
 set /p SERVICE=Service name: 
 set /p STARTTYPE=Startup type [Automatic, AutomaticDelayedStart, Manual, Disabled]: 
-set ARGS=-ServiceName "%SERVICE%" -SetStartupType "%STARTTYPE%"
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -SetStartupType "%STARTTYPE%"
+goto complete
 
 :terminate
 set /p SERVICE=Service name: 
-set ARGS=-ServiceName "%SERVICE%" -TerminateStuckProcess
-goto run
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" -ServiceName "%SERVICE%" -TerminateStuckProcess
+goto complete
 
-:run
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "Unblock-File -LiteralPath '%~dp0Windows_Service_Repair_Toolkit.ps1' -ErrorAction SilentlyContinue"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Windows_Service_Repair_Toolkit.ps1" %ARGS%
+:complete
 echo.
 pause
 goto menu
